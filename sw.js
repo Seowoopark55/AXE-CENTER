3ed47dc18b8bf3d91cec45823a4521b0b152f5e3
Seowoopark55 <dls18222@naver.com>
2026-07-06 14:55:46 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,26 @@
-const CACHE_NAME = 'axe-workcenter-v1-6-2';
-const APP_SHELL = ['./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-self.addEventListener('install', event => { self.skipWaiting(); event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).catch(()=>{})); });
-self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))); self.clients.claim(); });
+const CACHE_NAME = 'axe-workcenter-v1-7-1';
+const APP_SHELL = ['./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./axe.png','./AXE ALL.png'];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).catch(() => {}));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+
 self.addEventListener('fetch', event => {
   const req = event.request;
   if (req.method !== 'GET') return;
-  event.respondWith(fetch(req).then(res => { const copy=res.clone(); if(new URL(req.url).origin===location.origin){ caches.open(CACHE_NAME).then(cache=>cache.put(req,copy)).catch(()=>{}); } return res; }).catch(()=>caches.match(req).then(cached=>cached||caches.match('./index.html'))));
+  event.respondWith(
+    fetch(req).then(res => {
+      const copy = res.clone();
+      if (new URL(req.url).origin === location.origin) {
+        caches.open(CACHE_NAME).then(cache => cache.put(req, copy)).catch(() => {});
+      }
+      return res;
+    }).catch(() => caches.match(req).then(cached => cached || caches.match('./index.html')))
+  );
 });
